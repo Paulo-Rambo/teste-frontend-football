766f3c9cb1f04c64b307b9519c699c59a6df82e5
Paulo Vitor <patoassado@gmail.com>
2023-05-31 17:18:13 -0300

    feat: countrysPage mobile

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // Resto da configuração...
-
   setupFiles: ["dotenv/config"],
+  setupFilesAfterEnv: ["./testSetup.js"],
 };
--- src/pages/CountrysPage/countryList/styles.js
+++ src/pages/CountrysPage/countryList/styles.js
@@ -12,4 +12,14 @@ export const ListCards = styled.div`
     justify-content: center;
     width: 100%;
   }
+
+  @media (max-width: 420px) {
+    height: 65vh;
+    width: 90vw;
+    .sectionRow {
+      height: 100%;
+      flex-direction: column;
+      overflow-x: scroll;
+    }
+  }
 `;
